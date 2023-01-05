731cbc1d3cc49df3780a1f04b2799ab2545b4aff
hajungkim <hajung3406@naver.com>
2023-01-05 09:06:22 +0900

    Revert "0104 작업물"
    This reverts commit 40eba31e205a4035be8dd8c085d5a2014d4dc206.

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,4 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-
 export default function App({ Component, pageProps }) {
-  const client = new ApolloClient({
-    uri: "http://backendonline.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(),
-  });
-
-  return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+  return <Component {...pageProps} />;
 }
 //페이지가 아님! 모든 페이지에 공통 적용되는 설정들
