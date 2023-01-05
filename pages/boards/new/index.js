81ad4c670198887bb05719b36d6c4319da2f2ec1
hajungkim <hajung3406@naver.com>
2023-01-05 10:31:49 +0900

    게시물 등록하기 세션2 과제

--- pages/boards/new/index.js
+++ pages/boards/new/index.js
@@ -1,78 +1,170 @@
+import { useForm } from "react-hook-form";
+import { useMutation, gql } from "@apollo/client";
+
 import {
-  Address,
-  ButtonWrapper,
-  Contents,
-  ImageWrapper,
-  InputWrapper,
-  Label,
-  OptionWrapper,
-  Password,
-  RadioButton,
-  RadioLabel,
-  SearchButton,
-  Subject,
-  SubmitButton,
-  Title,
-  UploadButton,
   Wrapper,
-  Writer,
-  WriterWrapper,
-  Youtube,
-  Zipcode,
-  ZipcodeWrapper,
+  Label,
+  Input,
+  Textarea,
+  ImgUploader,
+  InputRadio,
+  Submit,
+  Error,
 } from "../../../styles/emotion";
 
-export default function BoardWriteUI() {
+const CREATE_BOARD = gql`
+  mutation createBoard(
+    $writer: String
+    $password: String
+    $title: String!
+    $contents: String!
+  ) {
+    createBoard(
+      createBoardInput: {
+        writer: $writer
+        password: $password
+        title: $title
+        contents: $contents
+      }
+    ) {
+      _id
+    }
+  }
+`;
+
+export default function New() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onBlur" });
+  const [createBoard] = useMutation(CREATE_BOARD);
+
+  // const [data, setData] = useState("");
+  const onSubmit = async (data, e) => {
+    if (data) {
+      const result = await createBoard({
+        variables: {
+          writer: data.writer,
+          password: data.password,
+          title: data.title,
+          contents: data.contents,
+        },
+      });
+      console.log(result);
+    }
+  };
+  const onError = (errors, e) => console.log(errors, e);
+
   return (
     <Wrapper>
-      <Title>게시글 등록</Title>
-      <WriterWrapper>
-        <InputWrapper>
-          <Label>작성자</Label>
-          <Writer type="text" placeholder="이름을 적어주세요." />
-        </InputWrapper>
-        <InputWrapper>
-          <Label>비밀번호</Label>
-          <Password type="password" placeholder="비밀번호를 작성해주세요." />
-        </InputWrapper>
-      </WriterWrapper>
-      <InputWrapper>
-        <Label>제목</Label>
-        <Subject type="text" placeholder="제목을 작성해주세요." />
-      </InputWrapper>
-      <InputWrapper>
-        <Label>내용</Label>
-        <Contents placeholder="내용을 작성해주세요." />
-      </InputWrapper>
-      <InputWrapper>
-        <Label>주소</Label>
-        <ZipcodeWrapper>
-          <Zipcode placeholder="07250" />
-          <SearchButton>우편번호 검색</SearchButton>
-        </ZipcodeWrapper>
-        <Address />
-        <Address />
-      </InputWrapper>
-      <InputWrapper>
-        <Label>유튜브</Label>
-        <Youtube placeholder="링크를 복사해주세요." />
-      </InputWrapper>
-      <ImageWrapper>
-        <Label>사진첨부</Label>
-        <UploadButton>+</UploadButton>
-        <UploadButton>+</UploadButton>
-        <UploadButton>+</UploadButton>
-      </ImageWrapper>
-      <OptionWrapper>
-        <Label>메인설정</Label>
-        <RadioButton type="radio" id="youtube" name="radio-button" />
-        <RadioLabel htmlFor="youtube">유튜브</RadioLabel>
-        <RadioButton type="radio" id="image" name="radio-button" />
-        <RadioLabel htmlFor="image">사진</RadioLabel>
-      </OptionWrapper>
-      <ButtonWrapper>
-        <SubmitButton>등록하기</SubmitButton>
-      </ButtonWrapper>
+      <h2>게시물 등록</h2>
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
+        <Label className="label_half first">
+          작성자
+          <Input
+            {...register("writer", {
+              required: "닉네임을 입력해주세요.",
+              pattern: {
+                value:
+                  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+                message: "@을 포함한 이메일 형식으로 작성해주세요",
+              },
+            })}
+            placeholder="이름을 적어주세요"
+          />
+          {<Error>{errors.writer?.message}</Error>}
+        </Label>
+        <Label className="label label_half">
+          비밀번호
+          <Input
+            type="password"
+            {...register("password", {
+              required: "비밀번호를 입력해주세요.",
+              pattern: {
+                value: /^.{8,}$/,
+                message: "8글자 이상으로 입력해주세요.",
+              },
+            })}
+            placeholder="비밀번호를 입력해주세요."
+          />
+          {<Error>{errors.password?.message}</Error>}
+        </Label>
+        <Label className="label">
+          제목
+          <Input
+            {...register("title", {
+              required: "제목을 작성해주세요.",
+            })}
+            placeholder="제목을 작성해주세요."
+          />
+          {<Error>{errors.title?.message}</Error>}
+        </Label>
+        <Label className="label">
+          내용
+          <Textarea
+            {...register("contents", {
+              required: "내용을 작성해주세요.",
+            })}
+            placeholder="내용을 작성해주세요."
+          />
+          {<Error>{errors.contents?.message}</Error>}
+        </Label>
+        <Label className="label">
+          주소
+          <div>
+            <Input className="postcode" {...register("postcode")} />
+            <button>우편번호 검색</button>
+          </div>
+          <Input
+            style={{
+              marginBottom: `30px !important`,
+              marginTop: `0px !important`,
+            }}
+            {...register("address1")}
+          />
+          <Input
+            {...register("address2")}
+            style={{
+              marginBottom: `37px !important`,
+              marginTop: `0px !important`,
+            }}
+          />
+        </Label>
+        <Label className="label">
+          유튜브
+          <Input {...register("youtube")} placeholder="링크를 복사해주세요." />
+        </Label>
+        <Label className="label">
+          사진 첨부
+          <div style={{ marginTop: 16 }}>
+            {[1, 2, 3].map((i) => {
+              return (
+                <ImgUploader key={i} {...register(`imgSrc${i}`)}>
+                  <button>upload</button>
+                </ImgUploader>
+              );
+            })}
+          </div>
+        </Label>
+        <Label className="label">
+          메인 설정
+          <InputRadio style={{ marginTop: 16 }}>
+            <input type="radio" {...register("mainset")} value="youtube" />
+            유튜브
+            <input
+              style={{ marginLeft: 22 }}
+              type="radio"
+              {...register("mainset")}
+              value="photo"
+            />
+            사진
+          </InputRadio>
+        </Label>
+        <div style={{ textAlign: "center" }}>
+          <Submit type="submit" value="등록하기" />
+        </div>
+      </form>
     </Wrapper>
   );
 }
--- styles/emotion.js
+++ styles/emotion.js
@@ -2,176 +2,209 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
   width: 1200px;
-  /* height: 1847px; */
-  border: 1px solid black;
+  padding: 60px 102px 100px;
   margin: 0 auto;
   margin-top: 100px;
-  padding-top: 80px;
-  padding-bottom: 100px;
-  padding-left: 102px;
-  padding-right: 102px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border: none;
-  box-shadow: 0px 0px 10px gray;
+  background: #ffffff;
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.2);
   box-sizing: border-box;
-`;
 
-export const Title = styled.div`
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 36px;
-  font-weight: bold;
+  > h2 {
+    font-family: "Noto Sans CJK KR";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 36px;
+    line-height: 53px;
+    text-align: center;
+    padding-bottom: 80px;
+  }
+  button {
+    padding: 14px 16px;
+    font-family: "Noto Sans CJK KR";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    background: #000;
+    color: #fff;
+    border: none;
+  }
 `;
-
-export const WriterWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  padding-top: 40px;
-`;
-
-export const Writer = styled.input`
-  width: 486px;
-  height: 52px;
-  padding-left: 16px;
-  border: 1px solid #bdbdbd;
-`;
-
-export const Password = styled.input`
-  width: 486px;
-  height: 52px;
-  padding-left: 16px;
-  border: 1px solid #bdbdbd;
-`;
-
 export const Label = styled.div`
-  padding-bottom: 16px;
-  font-size: 16px;
+  font-family: "Noto Sans CJK KR";
+  font-style: normal;
   font-weight: 500;
-`;
+  font-size: 16px;
+  line-height: 24px;
+  position: relative;
 
-export const InputWrapper = styled.div`
-  padding-top: 40px;
-  width: 100%;
+  &.label_half {
+    display: inline-block;
+    width: 486px;
+  }
+  &.first {
+    margin-right: 24px;
+  }
 `;
-
-export const Subject = styled.input`
-  width: 996px;
-  height: 52px;
-  padding-left: 16px;
-  border: 1px solid #bdbdbd;
-`;
-
-export const Contents = styled.textarea`
-  width: 996px;
-  height: 480px;
-  padding-left: 16px;
-  padding: 14px;
-  border: 1px solid #bdbdbd;
-`;
-
-export const ZipcodeWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-export const Zipcode = styled.input`
-  width: 77px;
-  height: 52px;
-  padding-left: 16px;
-  border: 1px solid #bdbdbd;
-`;
-
-export const SearchButton = styled.button`
-  width: 124px;
-  height: 52px;
-  margin-left: 16px;
-  background-color: black;
-  cursor: pointer;
-  color: white;
-`;
-
-export const Address = styled.input`
-  width: 996px;
-  height: 52px;
+export const Input = styled.input`
+  box-sizing: border-box;
   margin-top: 16px;
-  padding-left: 16px;
+  margin-bottom: 40px;
+  background: #ffffff;
   border: 1px solid #bdbdbd;
+  padding: 14px 16px;
+  width: 100%;
+  color: #000;
+  font-family: "Noto Sans CJK KR";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  &::placeholder {
+    color: #c4c4c4;
+    font-family: "Noto Sans CJK KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+  }
+  &.postcode {
+    width: 77px;
+    margin-right: 16px;
+    margin-bottom: 16px;
+  }
 `;
-
-export const Youtube = styled.input`
-  width: 996px;
-  height: 52px;
-  padding-left: 16px;
+export const Textarea = styled.textarea`
+  width: 100%;
+  height: 480px;
+  box-sizing: border-box;
+  padding: 14px 16px;
+  background: #ffffff;
   border: 1px solid #bdbdbd;
+  margin: 16px 0;
+  resize: none;
+  color: #000;
+  font-family: "Noto Sans CJK KR";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  margin-bottom: 40px;
+  &::placeholder {
+    color: #c4c4c4;
+    font-family: "Noto Sans CJK KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+  }
 `;
-
-export const ImageWrapper = styled.div`
-  width: 996px;
-  padding-top: 40px;
-`;
-
-export const UploadButton = styled.button`
+export const ImgUploader = styled.div`
+  box-sizing: border-box;
   width: 78px;
   height: 78px;
-  background-color: #bdbdbd;
+  background: #bdbdbd;
+  display: inline-block;
   margin-right: 24px;
-  outline: none;
+  margin-bottom: 30px;
+  text-align: center;
+  > button {
+    background: none !important;
+    font-family: "Noto Sans CJK KR";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 18px;
+    color: #4f4f4f;
+    padding: 0;
+    margin: 0;
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    padding: 42px 18px 18px 18px;
+    position: relative;
+    &:after {
+      content: "";
+      width: 14px;
+      height: 2px;
+      background: #4f4f4f;
+      position: absolute;
+      top: 29px;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    &:before {
+      content: "";
+      height: 14px;
+      width: 2px;
+      background: #4f4f4f;
+      position: absolute;
+      top: 23px;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  }
+`;
+export const InputRadio = styled.div`
+  > input[type="radio"] {
+    vertical-align: middle;
+    --webkit-appearance: none;
+    --moz-appearance: none;
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    border-radius: 100%;
+    background: #fff;
+    border: 1px solid #000;
+    margin: 0;
+    margin-right: 10px;
+  }
+  > input[type="radio"]:checked {
+    vertical-align: middle;
+    --webkit-appearance: none;
+    --moz-appearance: none;
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    border-radius: 100%;
+    background: #fff;
+    border: 1px solid #ffd600;
+    margin: 0;
+    margin-right: 10px;
+    padding: 4px;
+    position: relative;
+    &:after {
+      content: "";
+      width: 12px;
+      height: 12px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background: #ffd600;
+      border-radius: 100%;
+    }
+  }
+`;
+export const Submit = styled.input`
   border: none;
-  cursor: pointer;
-`;
-
-export const OptionWrapper = styled.div`
-  width: 996px;
-  padding-top: 40px;
-`;
-
-export const RadioButton = styled.input`
-  cursor: pointer;
-`;
-
-export const RadioLabel = styled.label`
-  margin-left: 8px;
-  margin-right: 20px;
+  display: inline-block;
+  background: #ffd600;
+  font-family: "Noto Sans CJK KR";
+  font-style: normal;
   font-weight: 500;
-  cursor: pointer;
-`;
-
-export const ButtonWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  padding-top: 80px;
-`;
-
-export const CancelButton = styled.button`
-  width: 179px;
-  height: 52px;
-  background-color: #bdbdbd;
-  border: none;
   font-size: 16px;
-  font-weight: 500;
-  margin-left: 12px;
-  margin-right: 12px;
+  line-height: 24px;
+  color: #000000;
+  padding: 14px 60px;
+  margin: 0 auto;
+  margin-top: 94px;
   cursor: pointer;
 `;
 
-export const SubmitButton = styled.button`
-  width: 179px;
-  height: 52px;
-  border: none;
-  font-size: 16px;
-  font-weight: 500;
-  margin-left: 12px;
-  margin-right: 12px;
-  cursor: pointer;
-
-  background-color: yellow;
-`;
-
-export const Error = styled.div`
-  padding-top: 10px;
-  font-size: 14px;
+export const Error = styled.small`
+  position: absolute;
+  left: 0;
+  bottom: 10px;
   color: red;
 `;
